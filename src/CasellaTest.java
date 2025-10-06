da59e6f65c7c7f3fd13a8b5329813fe9038c1938
Ivanguerra13 <167463663+Ivanguerra13@users.noreply.github.com>
2025-10-06 12:00:36 +0200

    Test constructor de Casella
    Primera versió del test del constructor de Casella sobre com ha de funcionar.

--- src/CasellaTest.java
+++ src/CasellaTest.java
@@ -5,8 +5,21 @@ import org.junit.jupiter.api.Test;
 class CasellaTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void Constructor() {
+		Posicio p1 = new Posicio(3,2);
+		Casella c1 = new Casella(p1);	
+		assertEquals(p,c1.getPosicio());
+		assertFalse(c1.isMina());
+		assertFalse(c1.isBandera());
+		assertFalse(c1.isDestapat());
+		try
+		{
+			c1 = new Casella(new Posicio(null));
+			assertTrue(false);
+		}catch(Exception e)
+		{
+			System.err.println(e.getMessage());
+		}
 	}
 
 }

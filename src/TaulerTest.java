57e59f1dffa365904af6af19d261f0cdad61bdbc
Ivanguerra13 <167463663+Ivanguerra13@users.noreply.github.com>
2025-10-06 18:01:05 +0200

    TestContstructorInicialTauler
    Al crear el constructor inicial de tauler ens vam donar compte que necesitavem un métode equals per a tauler , un altre per a casella i a més a més que la clase posició era redudant ja que podiem utilitzar un tauler de en forma de matriu on les posicions son les coordenades que pertanyen, es a dir la t[2][4] pertany a la posició 2,4.

--- src/TaulerTest.java
+++ src/TaulerTest.java
@@ -3,10 +3,19 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class TaulerTest {
-
+ 
+	Tauler [][] t1,t2;
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void ConstructorInicialTaulerTest()
+	{
+		for(int i=0;i<13;i++)
+			for(int j=0;j<13;j++)
+			{
+				t1[i][j] = new Casella();
+			}
+		t2 = new Tauler();
+		t1.equals(t2);
+		
 	}
 
 }

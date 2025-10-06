19b6c79f365e260bfd0367906b05eded8e465ecd
Ivanguerra13 <167463663+Ivanguerra13@users.noreply.github.com>
2025-10-06 18:18:07 +0200

    Actualitzar casellaTest
    Actualitzem casella test després d'eliminació de la clase posició

--- src/CasellaTest.java
+++ src/CasellaTest.java
@@ -4,32 +4,19 @@ import org.junit.jupiter.api.Test;
 
 class CasellaTest {
 	
-	Posicio p;
 	Casella c;
 	@Test
-	void Constructor() {
-		//Comprovem que el constructor genera correctament la casella amb la posició assingada, sense mina, sense bandera i tapada.
-		p = new Posicio(3,2);
-		c = new Casella(p);	
-		assertEquals(p,c.getPosicio());
+	void ConstructorCasellaTest() {
+		//Comprovem que el constructor genera correctament la casella sense mina, sense bandera i tapada.
+		c = new Casella();	
 		assertFalse(c.isMina());
 		assertFalse(c.isBandera());
 		assertFalse(c.isDestapat());
-		//Comprovem que dona una excepció si pasem null la posició.
-		try
-		{
-			c = new Casella(null);
-			assertTrue(false);
-		}catch(Exception e)
-		{
-			System.err.println(e.getMessage());
-		}
 	}
 		@Test
 		void PosarMinaTest() {
 			//Comprovem que l'assingació de la mina a una casella es fa correctament.
-			p = new Posicio(10,7);
-			c = new Casella(p);	
+			c = new Casella();	
 			c.setMina();
 			assertTrue(c.isMina());
 			
@@ -37,8 +24,7 @@ class CasellaTest {
 		@Test
 		void PosarAndTreureBanderaTest() {
 			//Comprovem que es pot posar banderas i treure correctament en una casella.
-			p = new Posicio(3,3);
-			c = new Casella(p);
+			c = new Casella();
 			c.changeBandera();
 			assertTrue(c.isBandera());
 			c.changeBandera();
@@ -49,8 +35,7 @@ class CasellaTest {
 		void DestaparCasellaTest()
 		{
 			//Comprovem que es pot destapar una casella tapada correctament.
-			p = new Posicio(7,10);
-			c = new Casella(p);
+			c = new Casella();
 			assertFalse(c.isDestapat());
 			c.destaparCasella();
 			assertTrue(c.isDestapat());

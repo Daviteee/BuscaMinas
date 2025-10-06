3be2d8506ae1c35dd544b2f9663bfec8dae46772
Ivanguerra13 <167463663+Ivanguerra13@users.noreply.github.com>
2025-10-06 12:12:37 +0200

    Primera implementació del codi intern de Casella
    Fem la primera implementació amb les variables privades que creiem que necesitem per que pasi el test i amb els mètodes necesaris també.

--- src/Casella.java
+++ src/Casella.java
@@ -1,4 +1,36 @@
-
 public class Casella {
 
+	private Posicio posCasella;
+	private boolean teMina;
+	private boolean teBandera;
+	private boolean estaDestapat;
+	
+	public Casella(Posicio p)
+	{
+		if(p == null)
+			throw new IllegalArgumentException("Error la posició de la casella no por ser null");
+		
+		posCasella = p;
+		teMina = false;
+		teBandera = false;
+		estaDestapat = false;
+	}
+	
+	public boolean isMina()
+	{
+		return teMina;
+	}
+	
+	public boolean isBandera()
+	{
+		return teBandera;
+	}
+	public boolean isDestapat()
+	{
+		return estaDestapat;
+	}
+	
+	public Posicio getPosicio() {
+		return posCasella;
+	}
 }

91a35c33d118e2fcf0c004aaf80ebbbe604be51f
Ivanguerra13 <167463663+Ivanguerra13@users.noreply.github.com>
2025-10-06 18:25:25 +0200

    Arreglar error test constructor de tauler
    Arreglem l'error de que haviem generat una matriu de taulers quan no era necesari i a més a més estavem utilitzant el mètode equals quan de moment no es necessari ni a tauler ni a casella.

--- src/TaulerTest.java
+++ src/TaulerTest.java
@@ -4,18 +4,19 @@ import org.junit.jupiter.api.Test;
 
 class TaulerTest {
  
-	Tauler [][] t1,t2;
+	Tauler t1,t2;
 	@Test
 	void ConstructorInicialTaulerTest()
 	{
-		for(int i=0;i<13;i++)
-			for(int j=0;j<13;j++)
+		//Comrpovem que el tauler inicial s'inicialitza correctament amb totes les caselles corresponents.
+		t1= new Tauler();
+		for(int i=0;i<12;i++)
+			for(int j=0;j<12;j++)
 			{
-				t1[i][j] = new Casella();
-			}
-		t2 = new Tauler();
-		t1.equals(t2);
-		
-	}
+				assertFalse(t1.getCasella(i,j).isBandera());
+				assertFalse(t1.getCasella(i,j).isMina());
+				assertFalse(t1.getCasella(i,j).isDestapat());
 
+			}
+	}
 }

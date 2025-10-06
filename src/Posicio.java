15d11ce7c4aa13c36604bbc49dfb6efc27eaf986
Ivanguerra13 <167463663+Ivanguerra13@users.noreply.github.com>
2025-10-06 11:19:37 +0200

    Primera implentacio del codi intern de Posicio
    Fem la primera implementacio del codi intern de Posicio a partir del  test de prova.

--- src/Posicio.java
+++ src/Posicio.java
@@ -1,4 +1,24 @@
 
 public class Posicio {
 
+	private int x;
+	private int y;
+	public Posicio(int x,int y)
+	{
+		if(x<0 || x>12 || y<0 || y>12)
+		{
+			throw new IllegalArgumentException("Els valors dels parametres de poscio son incorrectes");
+		}
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY()
+	{
+		return y;
+	}
 }

52deb54622464c1471a6effd4c6e62a1e5e86c83
Ivanguerra13 <167463663+Ivanguerra13@users.noreply.github.com>
2025-10-09 18:05:23 +0200

    Segona implementació constructor del tauler
    La primera tenia un error ja que no pasava el test i era perquè faltava l'inicialització de les posicions de la matriu ja que getCasella retornava un objecte null.

--- src/Tauler.java
+++ src/Tauler.java
@@ -1,9 +1,13 @@
+
 public class Tauler {
 
 	private Casella[][]tauler;
 	public Tauler()
 	{
 		tauler = new Casella[13][13];
+		for(int i = 0; i < 13; i++)
+			for(int j = 0; j < 13;j++)
+				tauler[i][j] = new Casella();
 		 		
 	}
 	

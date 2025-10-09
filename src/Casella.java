6c0b376cb48912544723c6b938eac29251ce2a30
Daviteee <davidmoraleslopez005@gmail.com>
2025-10-09 11:57:48 +0200

    Afegir l'atribut numMinesVoltant
    Hem afegit l'altribut numMinesVoltant a la classe Casella i hem afegit els metodes Getters i Setter de l'altribut amb les excepcions pertinents. Així com, comentaris explicatius a la classe Casella de tots els atributs i mètodes.

--- src/Casella.java
+++ src/Casella.java
@@ -1,48 +1,68 @@
 
 public class Casella {
 
-	private boolean teMina;
-	private boolean teBandera;
-	private boolean estaDestapat;
+	private boolean teMina; //Indica si la casella té m una mina o no
+	private boolean teBandera; //Indica si la casella té bandera o no
+	private boolean estaDestapat; //Indica si la casella esta destapada o no
+	private int numMinesVoltant; //Numero de mines al voltant de la casella
 	
-	public Casella()
-	{
-		
+	
+	public Casella(){
+		//Constructor per defecte de la classe Casella
 		teMina = false;
 		teBandera = false;
 		estaDestapat = false;
+		numMinesVoltant = 0;
 	}
 	
-	public boolean isMina()
-	{
+	public boolean isMina(){
+		//Getter de l'atribut teMina
 		return teMina;
 	}
 	
-	public void setMina()
-	{
+	public void setMina(){
+		//Setter de l'altribut teMina
 		teMina = true;
 	}
 	
-	public boolean isBandera()
-	{
+	public boolean isBandera(){
+		//Getter de l'altribut teBandera
 		return teBandera;
 	}
 	
-	public void changeBandera()
-	{
+	public void changeBandera(){
+		//Mètode per canviar l'atribut teBnadera (true a false i viceversa)
 		if(teBandera)
 			teBandera = false;
 		else
 			teBandera = true;
 	}
 	
-	public void destaparCasella()
-	{
-		estaDestapat= true;
+	public void destaparCasella(){
+		//Metode per destapar la casella
+		estaDestapat = true;
 	}
-	public boolean isDestapat()
-	{
+	
+	public boolean isDestapat(){
+		//Getter de l'atribut estaDestapat
 		return estaDestapat;
+	}
+	
+	public int getNumMinesVoltant() {
+		//Getter de l'atribut numMinesVoltant
+		return numMinesVoltant;
+	}
+	
+	public void setNumMinesVoltant(int num) {
+		//Setter de l'altribut numMinesVoltant
+		if(num < 0) {
+			throw new IllegalArgumentException("Nombre de mines menor a 0!!!");
+		}
+		if(num > 8) {
+			throw new IllegalArgumentException("Nombre de mines major a 8!!!");
+		}
+		
+		this.numMinesVoltant = num;
 	}
 	
 }

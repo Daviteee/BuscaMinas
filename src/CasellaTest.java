a6f452405de918f21bad2ecd842c991074fb0bf7
Ivanguerra13 <167463663+Ivanguerra13@users.noreply.github.com>
2025-10-09 10:51:06 +0200

    Afegir el mètode setUp a casellaTest
    Per evitar repetir codi afegim el mètode setUp que s'executa a cada mètode de la classe al principi de cadascuna.

--- src/CasellaTest.java
+++ src/CasellaTest.java
@@ -1,14 +1,20 @@
 import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CasellaTest {
 	
 	Casella c;
+	
+	@BeforeEach
+	void setUp()
+	{
+		c = new Casella();
+	}
+	
 	@Test
 	void ConstructorCasellaTest() {
 		//Comprovem que el constructor genera correctament la casella sense mina, sense bandera i tapada.
-		c = new Casella();	
 		assertFalse(c.isMina());
 		assertFalse(c.isBandera());
 		assertFalse(c.isDestapat());
@@ -16,7 +22,6 @@ class CasellaTest {
 		@Test
 		void PosarMinaTest() {
 			//Comprovem que l'assingació de la mina a una casella es fa correctament.
-			c = new Casella();	
 			c.setMina();
 			assertTrue(c.isMina());
 			
@@ -24,7 +29,6 @@ class CasellaTest {
 		@Test
 		void PosarAndTreureBanderaTest() {
 			//Comprovem que es pot posar banderas i treure correctament en una casella.
-			c = new Casella();
 			c.changeBandera();
 			assertTrue(c.isBandera());
 			c.changeBandera();
@@ -35,9 +39,10 @@ class CasellaTest {
 		void DestaparCasellaTest()
 		{
 			//Comprovem que es pot destapar una casella tapada correctament.
-			c = new Casella();
 			assertFalse(c.isDestapat());
 			c.destaparCasella();
 			assertTrue(c.isDestapat());
 		}
+		
+		
 }

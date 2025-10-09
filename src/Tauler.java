29fb5c3c88d1852f8a8fd4c1d9a27568e530709d
Ivanguerra13 <167463663+Ivanguerra13@users.noreply.github.com>
2025-10-09 19:17:05 +0200

    Afegir la comprovació dels límits a getCasella

--- src/Tauler.java
+++ src/Tauler.java
@@ -1,18 +1,21 @@
 
 public class Tauler {
 
+	private static final int MIDA = 13;
 	private Casella[][]tauler;
 	public Tauler()
 	{
-		tauler = new Casella[13][13];
-		for(int i = 0; i < 13; i++)
-			for(int j = 0; j < 13;j++)
+		tauler = new Casella[MIDA][MIDA];
+		for(int i = 0; i < MIDA; i++)
+			for(int j = 0; j < MIDA;j++)
 				tauler[i][j] = new Casella();
 		 		
 	}
 	
 	public Casella getCasella(int x,int y)
 	{
+		if(x > 12 || y > 12 || x < 0 || y < 0)
+			throw new IllegalArgumentException("Error la posició que demanes està fora dels límits del tauler");
 		return tauler[x][y];
 	}
 }

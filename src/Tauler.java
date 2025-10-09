d1f622aa6d5c850bb9564b071485b3e3baaeadeb
Ivanguerra13 <167463663+Ivanguerra13@users.noreply.github.com>
2025-10-09 18:00:50 +0200

    Primera implementacio contructor tauler i mètode getCasella
    Implementem una primera versió per veure si pasa el test del constructor del tauler.

--- src/Tauler.java
+++ src/Tauler.java
@@ -1,4 +1,14 @@
-
 public class Tauler {
 
+	private Casella[][]tauler;
+	public Tauler()
+	{
+		tauler = new Casella[13][13];
+		 		
+	}
+	
+	public Casella getCasella(int x,int y)
+	{
+		return tauler[x][y];
+	}
 }

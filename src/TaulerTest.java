cb28d299a66094618331627288a12f04ed8d436e
Daviteee <davidmoraleslopez005@gmail.com>
2025-10-15 17:01:33 +0200

    Actualitzar Test GetCasella
    Hem actualitzat el test del mètode getCasella afegint els testos ben implementats, afegint comentaris d'explicació del mètode de test.

--- src/TaulerTest.java
+++ src/TaulerTest.java
@@ -1,3 +1,4 @@
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -25,14 +26,202 @@ class TaulerTest {
 			}		
 	}
 	
+	
 	@Test
 	void getCasellaTest()
 	{
-		//Comprovem que el get casella controla correctament els limits del tauler.
+		// Test del GetCasella, mètode que apartir de dos parametres d'entrada (x i y) retorna la casella corresponent
+		// a la coordenada indicada per els parametres d'entrada dins del tauler.
+		// I comprova que el mètode getCasella fa un control correcte dels limits del tauler.
+		// Utilitzant testing amb particions equivalents, no es pot utilitzar Pairwise Testing ja que com el mètode
+		// reb només 2 parametres no es possible realitzar el pairwise ja que el resultat són totes les combinacions.
 		
-		assertNotNull(t1.getCasella(0, 0));
-		assertNotNull(t1.getCasella(12, 12));
+		assertTrue(t1.getCasella(0, 0) instanceof Casella);
+		assertTrue(t1.getCasella(0, 1) instanceof Casella);
+		assertTrue(t1.getCasella(0, 12) instanceof Casella);
+		assertTrue(t1.getCasella(0, 11) instanceof Casella);
+		assertTrue(t1.getCasella(0, 6) instanceof Casella);
 		
+		assertTrue(t1.getCasella(1, 0) instanceof Casella);
+		assertTrue(t1.getCasella(1, 1) instanceof Casella);
+		assertTrue(t1.getCasella(1, 12) instanceof Casella);
+		assertTrue(t1.getCasella(1, 11) instanceof Casella);
+		assertTrue(t1.getCasella(1, 6) instanceof Casella);
+		
+		assertTrue(t1.getCasella(12, 0) instanceof Casella);
+		assertTrue(t1.getCasella(12, 1) instanceof Casella);
+		assertTrue(t1.getCasella(12, 12) instanceof Casella);
+		assertTrue(t1.getCasella(12, 11) instanceof Casella);
+		assertTrue(t1.getCasella(12, 6) instanceof Casella);
+		
+		assertTrue(t1.getCasella(11, 0) instanceof Casella);
+		assertTrue(t1.getCasella(11, 1) instanceof Casella);
+		assertTrue(t1.getCasella(11, 12) instanceof Casella);
+		assertTrue(t1.getCasella(11, 11) instanceof Casella);
+		assertTrue(t1.getCasella(11, 6) instanceof Casella);
+		
+		assertTrue(t1.getCasella(6, 0) instanceof Casella);
+		assertTrue(t1.getCasella(6, 1) instanceof Casella);
+		assertTrue(t1.getCasella(6, 12) instanceof Casella);
+		assertTrue(t1.getCasella(6, 11) instanceof Casella);
+		assertTrue(t1.getCasella(6, 6) instanceof Casella);
+		
+		
+		try{
+			t1.getCasella(0, -1);
+			assertTrue(false);
+		}catch(Exception e){
+			System.err.println(e.getMessage());
+		}
+		
+		try{
+			t1.getCasella(0, 13);
+			assertTrue(false);
+		}catch(Exception e){
+			System.err.println(e.getMessage());
+		}
+		
+		try{
+			t1.getCasella(1, -1);
+			assertTrue(false);
+		}catch(Exception e){
+			System.err.println(e.getMessage());
+		}
+		
+		try{
+			t1.getCasella(1, 13);
+			assertTrue(false);
+		}catch(Exception e){
+			System.err.println(e.getMessage());
+		}
+		
+		try{
+			t1.getCasella(-1, 0);
+			assertTrue(false);
+		}catch(Exception e){
+			System.err.println(e.getMessage());
+		}
+		
+		try{
+			t1.getCasella(-1, 1);
+			assertTrue(false);
+		}catch(Exception e){
+			System.err.println(e.getMessage());
+		}
+		
+		try{
+			t1.getCasella(-1, -1);
+			assertTrue(false);
+		}catch(Exception e){
+			System.err.println(e.getMessage());
+		}
+		
+		try{
+			t1.getCasella(-1, 12);
+			assertTrue(false);
+		}catch(Exception e){
+			System.err.println(e.getMessage());
+		}
+		
+		try{
+			t1.getCasella(-1, 13);
+			assertTrue(false);
+		}catch(Exception e){
+			System.err.println(e.getMessage());
+		}
+		
+		try{
+			t1.getCasella(-1, 11);
+			assertTrue(false);
+		}catch(Exception e){
+			System.err.println(e.getMessage());
+		}
+		
+		try{
+			t1.getCasella(12, -1);
+			assertTrue(false);
+		}catch(Exception e){
+			System.err.println(e.getMessage());
+		}
+		
+		try{
+			t1.getCasella(12, 13);
+			assertTrue(false);
+		}catch(Exception e){
+			System.err.println(e.getMessage());
+		}
+		
+		try{
+			t1.getCasella(13, 0);
+			assertTrue(false);
+		}catch(Exception e){
+			System.err.println(e.getMessage());
+		}
+		
+		try{
+			t1.getCasella(13, 1);
+			assertTrue(false);
+		}catch(Exception e){
+			System.err.println(e.getMessage());
+		}
+		
+		try{
+			t1.getCasella(13, 12);
+			assertTrue(false);
+		}catch(Exception e){
+			System.err.println(e.getMessage());
+		}
+		
+		try{
+			t1.getCasella(13, 13);
+			assertTrue(false);
+		}catch(Exception e){
+			System.err.println(e.getMessage());
+		}
+		
+		try{
+			t1.getCasella(13, 11);
+			assertTrue(false);
+		}catch(Exception e){
+			System.err.println(e.getMessage());
+		}
+		
+		try{
+			t1.getCasella(13, 6);
+			assertTrue(false);
+		}catch(Exception e){
+			System.err.println(e.getMessage());
+		}
+		
+		try{
+			t1.getCasella(11, -1);
+			assertTrue(false);
+		}catch(Exception e){
+			System.err.println(e.getMessage());
+		}
+		
+		try{
+			t1.getCasella(11, 13);
+			assertTrue(false);
+		}catch(Exception e){
+			System.err.println(e.getMessage());
+		}
+		
+		try{
+			t1.getCasella(6, -1);
+			assertTrue(false);
+		}catch(Exception e){
+			System.err.println(e.getMessage());
+		}
+		
+		try{
+			t1.getCasella(6, 13);
+			assertTrue(false);
+		}catch(Exception e){
+			System.err.println(e.getMessage());
+		}
+		
+		//-------------------------------------
 		try
 		{
 			t1.getCasella(-1, 5);

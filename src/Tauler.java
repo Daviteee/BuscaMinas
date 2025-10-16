a726be704445e2fd9b2284b4a3954f1bfe5e6e04
Daviteee <davidmoraleslopez005@gmail.com>
2025-10-16 10:58:19 +0200

    Primera implementació del mètode generaMinesRandom
    Hem afegit la primera implementació del mètode generaMinesRandom a la classe Tauler. Funció que genera les mines de manera aleatòria dins del tauler, evitant la casella inicial del jugador i les 8 del voltant.

--- src/Tauler.java
+++ src/Tauler.java
@@ -1,4 +1,4 @@
-
+import java.util.Random;
 
 public class Tauler {
 
@@ -23,5 +23,31 @@ public class Tauler {
 	public void setMina(int x, int y) {
 
 		getCasella(x, y).setMina();
+	}
+	
+	public void generaMinesRandom(int xPlayer, int yPlayer) {
+		
+		if(xPlayer > 12 || yPlayer > 12 || xPlayer < 0 || yPlayer < 0){ 
+			//Comprobació dels limits del tauler segons les coordenades del clic del jugador.
+        	throw new IllegalArgumentException("Les coordenades del clic de l'usuari no són correctes (fora dels limits del tauler)");
+        }
+		
+		int nMines = 0; //Numero de mines al teuler.
+		
+		while(nMines < 10) { //Iterem fins que hi hagi 10 mines al tauler ben col·locades.
+			Random random = new Random();
+	        int xMina = random.nextInt(13); //Generació de x aleatòriament del 0 al 12.
+	        int yMina = random.nextInt(13); //Generació de y aleatòriament del 0 al 12.
+	        
+	        if( Math.abs(xMina - xPlayer) <= 1 && Math.abs(yMina - yPlayer) <= 1) { 
+	        	//La posició aleatòria generada de la mina esta dins de les 8 del voltant on el jugador a fet el clic inicial.
+	        	// La norma del buscamines diu que quan clica no pot haber-hi una mina en les 8 caselles del voltant. Per tant, tornem a generar.
+	        	continue;
+	        }
+	        
+	        getCasella(xMina, yMina).setMina(); //Posem la casella generada correctament com una mina (mina = true)
+	        nMines++; //Augmentem el nombre de mines ben col·locades al tauler.
+		}
+		
 	}
 }

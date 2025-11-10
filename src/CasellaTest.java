18814a0271a6360fee0e82df6e94f92189ca5d8e
Ivanguerra13 <167463663+Ivanguerra13@users.noreply.github.com>
2025-11-10 12:45:32 +0100

    Correcció error del commit anterior
    El commit anterior tenia una errata i era que haviem pujat sense voler la classe casella en comptes de la test.

--- src/CasellaTest.java
+++ src/CasellaTest.java
@@ -1,97 +1,78 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-public class Casella {
-
-	private boolean teMina; //Indica si la casella té m una mina o no
-	private boolean teBandera; //Indica si la casella té bandera o no
-	private boolean estaDestapat; //Indica si la casella esta destapada o no
-	private int numMinesVoltant; //Numero de mines al voltant de la casella
+class CasellaTest {
 	
+	Casella c;
 	
-	public Casella(){
-		//Constructor per defecte de la classe Casella
-		teMina = false;
-		teBandera = false;
-		estaDestapat = false;
-		numMinesVoltant = 0;
+	@BeforeEach
+	void setUp()
+	{
+		c = new Casella();
+	}
+		@Test
+		void PosarMinaTest() {
+			//Comprovem que l'assingació de la mina a una casella es fa correctament.
+			c.setMina();
+			assertTrue(c.isMina());
+			
+		}
+		@Test
+		void PosarAndTreureBanderaTest() {
+			//Comprovem que es pot posar banderas i treure correctament en una casella.
+			c.changeBandera();
+			assertTrue(c.isBandera());
+			c.changeBandera();
+			assertFalse(c.isBandera());
+		}
 		
-		//Postcondició del constructor per comprovar que s'han creat els objectes correctament
-		assert(teMina == false) : "Error la casella s'ha iniciat amb mina";
-		assert(teBandera == false) : "Error la casella s'ha iniciat amb bandera";
-		assert(estaDestapat == false) : "Error la casella s'ha iniciat destapada";
-		assert(numMinesVoltant == 0) : "Error la casella s'ha iniciat amb un número de mines al seu voltant";
-
-	}
-	
-	public boolean isMina(){
-		//Getter de l'atribut teMina
-		return teMina;
-	}
-	
-	public void setMina(){
-		//Setter de l'altribut teMina
-		//Precondició per veure si la casella on vols posar la mina no hi ha mina.
-		assert(teMina == false) : "Error vols posar una mina a una casella que ja té una";
-		teMina = true;	
-		//Postcondició del setter per comprovar que la mina ha sigut col·locada correctament
-		assert(teMina == true) : "Error la mina no s'ha col·locat correctament a la casella";
-
-	}
-	
-	public boolean isBandera(){
-		//Getter de l'altribut teBandera
-		return teBandera;
-	}
-	
-	public void changeBandera(){
-		//Mètode per canviar l'atribut teBnadera (true a false i viceversa)
-		if(teBandera)
+		@Test
+		void DestaparCasellaTest()
 		{
-			teBandera = false;
-			//Postcondició per comprovar que la bandera s'ha tret correctament de la casella
-			assert(teBandera == false) : "Error la bandera no s'ha tret correctament a la casella";
-
-		}
-		else
-		{
-			teBandera = true;
-			//Postcondició per comprovar que la bandera s'ha col·locat correctament de la casella
-			assert(teBandera == true) : "Error la bandera s'ha col·locat correctament a la casella";
-		}
-	}
-	
-	public void destaparCasella(){
-		//Mètode per destapar la casella
-		//Precondició per veure si la casella on vols destapar està tapada.
-		assert(estaDestapat == false) : "Error vols destapar una casella que ja està destapada";
-		estaDestapat = true;
-		
-		//Postcondició de destapar la casella per comprovar que la casella s'ha destapat correctament
-		assert(estaDestapat == true) : "Error la casella no s'han destapat correctament";
-	}
-	
-	public boolean isDestapat(){
-		//Getter de l'atribut estaDestapat
-		return estaDestapat;
-	}
-	
-	public int getNumMinesVoltant() {
-		//Getter de l'atribut numMinesVoltant
-		return numMinesVoltant;
-	}
-	
-	public void setNumMinesVoltant(int num) {
-		//Setter de l'altribut numMinesVoltant
-		//Precondició per veure que el número de mines a instaurar es correcte (entre 0 y 8)
-		if(num < 0) {
-			throw new IllegalArgumentException("Nombre de mines menor a 0!!!");
-		}
-		if(num > 8) {
-			throw new IllegalArgumentException("Nombre de mines major a 8!!!");
+			//Comprovem que es pot destapar una casella tapada correctament.
+			assertFalse(c.isDestapat());
+			c.destaparCasella();
+			assertTrue(c.isDestapat());
 		}
 		
-		this.numMinesVoltant = num;
-		//Post condició per veure si el número de mines al voltant s'ha instaurat correctament
-		assert(numMinesVoltant == num) : "Error el número de mines al voltant no s'ha instaurat correctament";
-	}
-	
+		@Test
+		void setNumMinesVoltantTest() {
+			
+			//Comprovem que es pot posar el nombre de mines al voltant correctament.
+			//Particions equivalents: (0 - 8): Particions equivalents
+			c.setNumMinesVoltant(0);
+			assertEquals(0, c.getNumMinesVoltant()); //Valor frontera
+			
+			c.setNumMinesVoltant(8);
+			assertEquals(8, c.getNumMinesVoltant()); //Valor frontera
+			
+			
+			try {
+				c.setNumMinesVoltant(-1); //Valor limit inferior
+				assertTrue(false);
+				
+			}catch(Exception e) {
+				System.err.println(e.getMessage());
+			}
+			
+			try {
+				c.setNumMinesVoltant(9); //Valor limit superior
+				assertTrue(false);
+				
+			}catch(Exception e) {
+				System.err.println(e.getMessage());
+			}
+			
+			c.setNumMinesVoltant(1);
+			assertEquals(1, c.getNumMinesVoltant()); //Valor limit superior
+			
+			c.setNumMinesVoltant(4);
+			assertEquals(4, c.getNumMinesVoltant()); //Valor interior
+			
+			c.setNumMinesVoltant(7);
+			assertEquals(7, c.getNumMinesVoltant()); //Valor limit inferior
+			
+		}
+		
 }

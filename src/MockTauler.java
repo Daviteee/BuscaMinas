005d0aa740dda9aba8eb31368d0243f387c14582
Daviteee <davidmoraleslopez005@gmail.com>
2025-11-10 13:06:29 +0100

    Declaració MockO Object del Tauler
    Afegim el Mock Object en un fitxer apart per seguir una estructura més neta. És la primera versió del mock object, que utilitzarem en els diferents casos de prova de la classe Tauler.

---
+++ src/MockTauler.java
@@ -0,0 +1,10 @@
+
+public class MockTauler extends Tauler{
+	
+	// Mock object de la classe Tauler per facilitar els casos de prova que requereixen generació de taulers especifics.
+	// Com la generació de mines es random, necessitem taulers amb les mines posicionades manualment per nosaltres.
+	
+	public Tauler getTauler(int cas) {
+		return null;
+	}
+}

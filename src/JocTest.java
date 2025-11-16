8ffc9d6d88e7347a84189d2eeee1ad40d87a427c
Daviteee <davidmoraleslopez005@gmail.com>
2025-11-16 09:52:08 +0100

    Primera versió clicDretTest
    Primera versió del test del mètode cliclDret de la classe Joc.

--- src/JocTest.java
+++ src/JocTest.java
@@ -3,10 +3,16 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class JocTest {
+	
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    public void clicDretTest() {
+		// Test que comproba que el clic Dret possa la bandera correctament.
+        Joc joc = new Joc();
+        joc.clicDret(2, 2); // Posem bandera a la posició 2,2
+        assertTrue(joc.getTauler().getCasella(2, 2).isBandera()); // Comprovem que la bandera s'ha possat
+    }
+	
+	
 
 }

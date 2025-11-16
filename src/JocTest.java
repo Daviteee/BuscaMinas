d3c44fb65db54c924984e1d8f8869e5e4960e32f
Daviteee <davidmoraleslopez005@gmail.com>
2025-11-16 10:32:14 +0100

    Afegir casos de proba a clicDret i primera versió getTaulerTest
    Hem afegit un cas de prova al test de clicDret i hem implementat la primera versió del mètode de test de getTauler de la classe Joc.

--- src/JocTest.java
+++ src/JocTest.java
@@ -11,8 +11,20 @@ class JocTest {
         Joc joc = new Joc();
         joc.clicDret(2, 2); // Posem bandera a la posició 2,2
         assertTrue(joc.getTauler().getCasella(2, 2).isBandera()); // Comprovem que la bandera s'ha possat
+        
+        // Cas on no podrem possar una bandera ja que la casella ja ha estat destapada
+        joc.getTauler().getCasella(0,1).setMina(); // Posem una mina a la casella 0,1
+        joc.getTauler().destapaCasella(1,1); // Destapem la casella 1,1 (per tant tindrá el numero de mines al voltant = 1)
+        assertFalse(joc.getTauler().getCasella(1, 1).isBandera()); // Comprovem que no podem col·locar bandera ja que esta destapada
+        
     }
 	
+	@Test
+	public void getTaulerTest() {
+		Tauler t1 = new Tauler();
+		Joc joc = new Joc(t1); // Constructor per parametres
+		assertEquals(t1, joc.getTauler());
+	}
 	
 
 }

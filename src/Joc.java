a240972303d642ba754d440946412d26bcdd12a9
Daviteee <davidmoraleslopez005@gmail.com>
2025-11-16 10:44:56 +0100

    Implementació getTauler
    Hem implementat la primera versió del mètode getTauler de la classe Joc.

--- src/Joc.java
+++ src/Joc.java
@@ -4,6 +4,10 @@ public class Joc {
 	private Tauler tauler;
     private boolean partidaAcabada;
     
+    public Tauler getTauler() {
+    	return this.tauler;
+    }
+    
     public void clicDret(int x, int y) {
         if (partidaAcabada)
             return;

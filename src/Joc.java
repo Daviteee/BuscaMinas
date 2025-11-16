a4b3117a559f7915c144c3b1bd8489c8bc018f5e
Daviteee <davidmoraleslopez005@gmail.com>
2025-11-16 09:59:24 +0100

    Primera versió clicDret
    Hem implementat la primera versió del mètode clicDret de la classe Joc, aquest mètode permet col·locar una bandera al tauler de joc.

--- src/Joc.java
+++ src/Joc.java
@@ -1,4 +1,14 @@
 
 public class Joc {
+	
+	private Tauler tauler;
+    private boolean partidaAcabada;
+    
+    public void clicDret(int x, int y) {
+        if (partidaAcabada)
+            return;
+
+        tauler.changeBandera(x, y);
+    }
 
 }

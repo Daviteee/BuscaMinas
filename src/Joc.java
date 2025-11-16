86c7edfbeb1341520b85757e5ab492cc474dcf95
Daviteee <davidmoraleslopez005@gmail.com>
2025-11-16 10:54:00 +0100

    Implementació constructors de la classe Joc
    Hem implementat la primera versió dels constructors de la classe Joc, tant el per parametres com el per defecte.

--- src/Joc.java
+++ src/Joc.java
@@ -4,6 +4,16 @@ public class Joc {
 	private Tauler tauler;
     private boolean partidaAcabada;
     
+    public Joc(){ // Constructor per defecte
+    	this.partidaAcabada = false;
+    	this.tauler = null;
+    }
+    
+    public Joc(Tauler t) { // Constructor per parametres
+    	this.partidaAcabada = false;
+    	this.tauler = t;
+    }
+    
     public Tauler getTauler() {
     	return this.tauler;
     }

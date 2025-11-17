a2810eafd1d5fb3a7b0df0c89d8bd4925678edc8
Daviteee <davidmoraleslopez005@gmail.com>
2025-11-17 16:44:08 +0100

    Afegir test getVista i correcció BUGS
    Hem corretgit multiples errors que hi havia al codi de test en les crides del constructor per parametres. També hem implementat el codi de test del getter de la vista de Joc, també implementarem un setter, del qual no farem test ja que es identic al del getter i sería redundant.

--- src/JocTest.java
+++ src/JocTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.Test;
 
 import model.Tauler;
+import vista.BuscaminesVista;
 
 class JocTest {
 	
@@ -26,7 +27,7 @@ class JocTest {
     public void clicDretTest() {
 		// Test que comproba que el clic Dret possa la bandera correctament.
 		Tauler t1 = new Tauler();
-        Joc joc = new Joc(t1);
+        Joc joc = new Joc(t1, 13);
         joc.clicDret(2, 2); // Posem bandera a la posició 2,2
         assertTrue(joc.getTauler().getCasella(2, 2).isBandera()); // Comprovem que la bandera s'ha possat
         
@@ -40,14 +41,22 @@ class JocTest {
 	@Test
 	public void getTaulerTest() {
 		Tauler t1 = new Tauler();
-		Joc joc = new Joc(t1); // Constructor per parametres
+		Joc joc = new Joc(t1, 13); // Constructor per parametres
 		assertEquals(t1, joc.getTauler());
+	}
+	
+	@Test
+	public void getVistaTest() {
+		Joc joc = new Joc();
+		BuscaminesVista v = new BuscaminesVista(joc, 13);
+		joc.setVista(v);
+		assertEquals(v, joc.getVista());
 	}
 	
 	@Test
 	public void clicEsquerraTest() {
 		Tauler t1 = new Tauler();
-		Joc joc = new Joc(t1);
+		Joc joc = new Joc(t1, 13);
 		joc.clicEsquerra(0,0); // Destapem la casella 0,0
 		assertTrue(joc.getTauler().getCasella(0,0).isDestapat());
 	}

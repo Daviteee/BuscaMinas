d3c4a296417de377790742146091194746da638b
Ivanguerra13 <167463663+Ivanguerra13@users.noreply.github.com>
2025-12-01 12:58:41 +0100

    Modificació tauler
    Hem afegit al tauler un invariant més l'atribut pell correcte funcionament del Mock

--- src/Tauler.java
+++ src/Tauler.java
@@ -1,35 +1,53 @@
-
-
+package model;
 import java.util.Random;
 
 public class Tauler {
 
 	private static final int MIDA = 13;
 	protected Casella[][]tauler;
+	int nMaxMines;
+	Random myRandom; //Atribut per poder utilitzar el MockObject de Random i generar les mines a on volem.
 	
-	public Tauler()
-	{
+	public Tauler(Random r){
+		this.myRandom = r;
 		tauler = new Casella[MIDA][MIDA];
 		for(int i = 0; i < MIDA; i++)
 			for(int j = 0; j < MIDA;j++)
 				tauler[i][j] = new Casella();
-		 		
+		this.nMaxMines = 30;
+		//Postcondicions per comporvar que s'ha inicialitzat correctament en 30 mines i el myRandom
+		assert(this.nMaxMines == 30) : "Error el número màxim de mines no s'ha incialitzat correctament";
+		assert(this.myRandom == r) : "Error l'atribut del tipus Random no s'ha inicialitzat correctament";
 	}
 	
-	public Casella getCasella(int x,int y)
-	{
+	public void invariant() {
+		assert(nMaxMines > 0): "Error el número màxim de mines es menor a 0";
+		assert(myRandom != null): "Error l'atribut del tipus Random és null";
+	}
+	
+	public Casella getCasella(int x,int y){
+		invariant();
 		//Precondició dels límits del tauler.
 		if(x > 12 || y > 12 || x < 0 || y < 0)
 			throw new IllegalArgumentException("Error la posició que demanes està fora dels límits del tauler");
+		invariant();
 		return tauler[x][y];
 	}
 	
 	public boolean isMina(int x, int y) {
+		invariant();
 		return getCasella(x, y).isMina();
 	}
-	
-	protected void generaMinesRandom(int xPlayer, int yPlayer) {
-		
+	public void setMaxMines(int n){
+		invariant();
+		nMaxMines = n;
+		//Postcondició per comporvar que s´ha fet correctament el setter
+		assert(nMaxMines == n) : "Error el número màxim de mines no concideix amb el paràmetre rebut al setter";
+		invariant();
+	}
+
+	public void generaMinesRandom(int xPlayer, int yPlayer) {
+		invariant();
 		if(xPlayer > 12 || yPlayer > 12 || xPlayer < 0 || yPlayer < 0){ 
 			//Precondició dels límits del tauler segons les coordenades del clic del jugador.
         	throw new IllegalArgumentException("Les coordenades del clic de l'usuari no són correctes (fora dels limits del tauler)");
@@ -37,7 +55,7 @@ public class Tauler {
 		
 		int nMines = 0; //Número de mines al teuler.
 		
-		while(nMines < 10) { //Iterem fins que hi hagi 10 mines al tauler ben col·locades.
+		while(nMines < nMaxMines) { //Iterem fins que hi hagi 30 mines al tauler ben col·locades.
 			Random random = new Random();
 	        int xMina = random.nextInt(13); //Generació de x aleatòriament del 0 al 12.
 	        int yMina = random.nextInt(13); //Generació de y aleatòriament del 0 al 12.
@@ -54,25 +72,28 @@ public class Tauler {
 	        }
 	        
 		}	
+		invariant();
 	}
 	
 	public void changeBandera(int x, int y) {
 		// Canviem l'atribut bandera de la casella indicada (si era true -> false, i al revés).
 		//Utilitzem el mètode getCasella en comptes d'accedir a casella[][]
-		//per evitar tornar a fer la precondició del límit del tauler pels valor de paràmetres x,y.
+		//per evitar tornar a fer la precondició del límit del tauler pels valor de paràmetres x,y i més comprovem l'invariant.
 		getCasella(x, y).changeBandera();
 	}
 	
 	public void setNumMinesVoltant() {
+		invariant();
 		for(int i = 0; i < MIDA; i++)
 			for(int j = 0; j < MIDA;j++) {
 				int mines = comptarNumMines(i, j);
 	            tauler[i][j].setNumMinesVoltant(mines);
 			}
-				
+		invariant();
 	}
 	
 	private int comptarNumMines(int x, int y) {
+		invariant();
 	    int count = 0;
 
 	    for (int dx = -1; dx <= 1; dx++) {
@@ -88,12 +109,12 @@ public class Tauler {
 	            }
 	        }
 	    }
+	    invariant();
 	    return count;
 	}
 	
 	public void destapaCasella(int x, int y) {
-		
-	    Casella c = getCasella(x, y);
+	    Casella c = getCasella(x, y); //Comprova els límits i l'invariant
 
 	    // Si ja está destapada o té bandera, no fem res.
 	    if (c.isDestapat() || c.isBandera()) {
@@ -125,18 +146,22 @@ public class Tauler {
 	            }
 	        }
 	    }
+	    invariant();
 	}
 	
 	public boolean totesDestapadesSenseMines() {
+		invariant();
 		// Mètode que comprova que totes les caselles que no son mines estan destapades (per comprovar que ha guanyat el jugador o no)
 	    for (int i = 0; i < MIDA; i++) {
 	        for (int j = 0; j < MIDA; j++) {
 	            Casella c = getCasella(i, j);
 	            if (!c.isMina() && !c.isDestapat()) {
+	            	invariant();
 	                return false;
 	            }
 	        }
 	    }
+	    invariant();
 	    return true;
 	}
 }

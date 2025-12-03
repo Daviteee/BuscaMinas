f8f3531a5a1ec69bb640393a08927854695bae3e
Daviteee <davidmoraleslopez005@gmail.com>
2025-12-03 12:44:37 +0100

    Afegir casos a CasellaTest
    Hem afegit casos de test al testing de Casella per aconseguir el coverage correcte. Cal comentar que el % de coverage de les classes Tauler i Casella no es del 100% degut a uns asserts que no es poden comprovar ja que son postcondicions de constructors o setters.

--- src/model/CasellaTest.java
+++ src/model/CasellaTest.java
@@ -13,68 +13,78 @@ class CasellaTest {
 	{
 		c = new Casella();
 	}
-		@Test
-		void PosarMinaTest() {
-			//Comprovem que l'assingació de la mina a una casella es fa correctament.
-			c.setMina();
-			assertTrue(c.isMina());
-			
-		}
-		@Test
-		void PosarAndTreureBanderaTest() {
-			//Comprovem que es pot posar banderas i treure correctament en una casella.
-			c.changeBandera();
-			assertTrue(c.isBandera());
-			c.changeBandera();
-			assertFalse(c.isBandera());
+		
+	@Test
+	void PosarMinaTest() {
+		//Comprovem que l'assingació de la mina a una casella es fa correctament.
+		c.setMina();
+		assertTrue(c.isMina());
+		try {
+			c.setMina(); // Tornem a possar una mina a una casella que ja té mina.
+		}catch(AssertionError e) {
+			System.err.println(e.getMessage());
 		}
 		
-		@Test
-		void DestaparCasellaTest()
-		{
-			//Comprovem que es pot destapar una casella tapada correctament.
-			assertFalse(c.isDestapat());
-			c.destaparCasella();
-			assertTrue(c.isDestapat());
-		}
+	}
+	@Test
+	void PosarAndTreureBanderaTest() {
+		//Comprovem que es pot posar banderas i treure correctament en una casella.
+		c.changeBandera();
+		assertTrue(c.isBandera());
+		c.changeBandera();
+		assertFalse(c.isBandera());
+	}
 		
-		@Test
-		void setNumMinesVoltantTest() {
-			
-			//Comprovem que es pot posar el nombre de mines al voltant correctament.
-			//Particions equivalents: (0 - 8): Particions equivalents
-			c.setNumMinesVoltant(0);
-			assertEquals(0, c.getNumMinesVoltant()); //Valor frontera
-			
-			c.setNumMinesVoltant(8);
-			assertEquals(8, c.getNumMinesVoltant()); //Valor frontera
-			
-			
-			try {
-				c.setNumMinesVoltant(-1); //Valor limit inferior
-				assertTrue(false);
-				
-			}catch(Exception e) {
-				System.err.println(e.getMessage());
-			}
-			
-			try {
-				c.setNumMinesVoltant(9); //Valor limit superior
-				assertTrue(false);
-				
-			}catch(Exception e) {
-				System.err.println(e.getMessage());
-			}
-			
-			c.setNumMinesVoltant(1);
-			assertEquals(1, c.getNumMinesVoltant()); //Valor limit superior
-			
-			c.setNumMinesVoltant(4);
-			assertEquals(4, c.getNumMinesVoltant()); //Valor interior
-			
-			c.setNumMinesVoltant(7);
-			assertEquals(7, c.getNumMinesVoltant()); //Valor limit inferior
-			
+	@Test
+	void DestaparCasellaTest(){
+		//Comprovem que es pot destapar una casella tapada correctament.
+		assertFalse(c.isDestapat());
+		c.destaparCasella();
+		assertTrue(c.isDestapat());
+		
+		try {
+			c.destaparCasella(); // Tornem a destapar una casella que ja está destapada.
+		}catch(AssertionError e) {
+			System.err.println(e.getMessage());
 		}
+	}
+		
+	@Test
+	void setNumMinesVoltantTest() {
+			
+		//Comprovem que es pot posar el nombre de mines al voltant correctament.
+		//Particions equivalents: (0 - 8): Particions equivalents
+		c.setNumMinesVoltant(0);
+		assertEquals(0, c.getNumMinesVoltant()); //Valor frontera
+			
+		c.setNumMinesVoltant(8);
+		assertEquals(8, c.getNumMinesVoltant()); //Valor frontera
+				
+		try {
+			c.setNumMinesVoltant(-1); //Valor limit inferior
+			assertTrue(false);
+				
+		}catch(Exception e) {
+			System.err.println(e.getMessage());
+		}
+			
+		try {
+			c.setNumMinesVoltant(9); //Valor limit superior
+			assertTrue(false);
+				
+		}catch(Exception e) {
+			System.err.println(e.getMessage());
+		}
+			
+		c.setNumMinesVoltant(1);
+		assertEquals(1, c.getNumMinesVoltant()); //Valor limit superior
+		
+		c.setNumMinesVoltant(4);
+		assertEquals(4, c.getNumMinesVoltant()); //Valor interior
+			
+		c.setNumMinesVoltant(7);
+		assertEquals(7, c.getNumMinesVoltant()); //Valor limit inferior
+			
+	}
 		
 }

3a30feb5f069e34994380f08ef7672daaeb6328a
Daviteee <davidmoraleslopez005@gmail.com>
2025-12-04 19:14:37 +0100

    Afegir casos de prova clicEsquerra
    Hem afegit casos de prova al test del mètode cliclEsquerra de la classe Joc. Ens hem adonat de que requerim del mètode isDestapat a la classe Joc.

--- src/controlador/JocTest.java
+++ src/controlador/JocTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Random;
 import org.junit.jupiter.api.Test;
+
+import model.MockRandom;
 import model.Tauler;
 import vista.BuscaminesVista;
 import vista.MockBuscaminesVista;
@@ -45,7 +47,7 @@ class JocTest {
 		// Casos posibles: Posem la bandera en una casella tapada, posem una bandera en una casella destapada,
 		// treiem una bandera (clic dret sobre una bandera)
 		
-		// Test que comproba que el clic dret posa la bandera en una casella tapada (correcte).
+		// Cas que comproba que el clic dret posa la bandera en una casella tapada (correcte).
 		Random r = new Random();
 		Tauler t1 = new Tauler(r);
         Joc joc = new Joc(t1);
@@ -68,8 +70,7 @@ class JocTest {
 		joc.crearVistaDelJoc(vista);
 		joc.clicEsquerra(2, 2);
         joc.clicDret(2, 2); // Posem bandera a la posició 2,2 (que esta destapada)
-        assertFalse(joc.isBandera(2,2)); // Comprovem que la bandera s'ha possat
-        
+        assertFalse(joc.isBandera(2,2)); // Comprovem que la bandera no s'ha possat
     }
 	
 	@Test
@@ -81,25 +82,46 @@ class JocTest {
 	}
 	
 	@Test
-	public void getVistaTest() {
-		Random r = new Random();
-		Tauler t1 = new Tauler(r);
-		Joc joc = new Joc(t1);
-		BuscaminesVista v = new BuscaminesVista(joc);
-		joc.setVista(v);
-		assertEquals(v, joc.getVista());
-	}
-	
-	@Test
 	public void clicEsquerraTest() {
+		// El clic esquerra significa destapar una casella
+		// Casos posibles: Destapem la primera casella (mai té mina), destapem una casella ja destapada, 
+		// destapem una casella amb mina (incorrecte), destapem una casella amb bandera (incorrecte).
+		
+		// Cas on destapem la primera casella
 		Random r = new Random();
 		Tauler t1 = new Tauler(r);
         Joc joc = new Joc(t1);
         BuscaminesVista vista  = new MockBuscaminesVista(joc); //Creem un mock de la vista.
 		vista.initVista(); //La vista és genera a partir del mock (no fa res)
 		joc.crearVistaDelJoc(vista); 
-		joc.clicEsquerra(0,0); // Destapem la casella 0,0
-		assertTrue(joc.getTauler().getCasella(0,0).isDestapat());
+		joc.clicEsquerra(6,6); // Destapem la casella 6,6 (com a primera casella)
+		assertTrue(joc.isDestapat(6, 6)); // Comprovem que la casella s'ha destapat.
+		assertFalse(joc.getPartidaAcabada()); // Comprovem que la partida no s'ha acabat.
+		
+		// Cas on destapem una casella ja destapada
+		joc.clicEsquerra(6, 6); // Tornem a clicar en la mateixa casella anterior
+		assertTrue(joc.isDestapat(6, 6)); // La casella ha de seguir destapada (no ha de fer res).
+		
+		// Cas on destapem una casella que té una bandera (no es pot):
+		joc.clicDret(0, 0); // Posem una bandera a la casella 0, 0.
+		joc.clicEsquerra(0, 0); // Intentem destapar la casella 0, 0.
+		assertFalse(joc.isDestapat(0, 0)); // Comprovem que no s'ha destapat la casella amb la bandera.
+		
+		// Cas on destapem una mina (la casella SI que es destapa, però la partida acaba)
+		r = new MockRandom(3, 0);
+		t1 = new Tauler(r);
+		t1.generaMinesRandom(0, 0);
+		t1.setMaxMines(1);
+        joc = new Joc(t1);
+        vista  = new MockBuscaminesVista(joc); //Creem un mock de la vista.
+		vista.initVista(); //La vista és genera a partir del mock (no fa res)
+		joc.crearVistaDelJoc(vista); 
+		joc.clicEsquerra(12,5); // Destapem la casella 12,5 (casella amb mina, predefinida pel MockRandom)
+		assertTrue(joc.isDestapat(12, 5)); // Comprovem que la casella s'ha destapat.
+		assertTrue(joc.getPartidaAcabada()); // Comprovem que la partida s'ha acabat.
+		
+		
+		
 	}
 	
 

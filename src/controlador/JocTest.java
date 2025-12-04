4ab80b5f6394cfdebd7fd0d2914a8e081e591e69
Daviteee <davidmoraleslopez005@gmail.com>
2025-12-04 18:52:11 +0100

    Afegir casos de prova cliclDret
    Hem afegit els casos de prova al test del mètode clicDret de la classe Joc, ens hem adonat que requerim de la funció isBandera ala classe Joc per poder complir el principi de dont talk to strangers. També, ens hem adonat de petits bugs a la classe Joc que corretgirem acontinuació.

--- src/controlador/JocTest.java
+++ src/controlador/JocTest.java
@@ -1,3 +1,4 @@
+
 package controlador;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,7 +22,7 @@ class JocTest {
 		
 		//Afegim la vista al controlador (el mockObject). Comprovem que s'inicia correctament la vista al controlador. Això es comprova mitjançant postcondicions
 		//al codi desenvolupat si no surt error està correcte.
-		joc.crearVistaDelJoc(vista); 
+		joc.crearVistaDelJoc(vista);
 		
 		//Comprovació de les excepcions que han de sortir al constructor. Si tauler és null o si la vista és null. Aquestes comrpovacions es fan al codi desenvolupat
 		//mitjançant precondicions.
@@ -40,21 +41,34 @@ class JocTest {
 
 	@Test
     public void clicDretTest() {
-		// Test que comproba que el clic Dret possa la bandera correctament.
+		// El clic dret significa posar una bandera a una casella
+		// Casos posibles: Posem la bandera en una casella tapada, posem una bandera en una casella destapada,
+		// treiem una bandera (clic dret sobre una bandera)
+		
+		// Test que comproba que el clic dret posa la bandera en una casella tapada (correcte).
 		Random r = new Random();
 		Tauler t1 = new Tauler(r);
         Joc joc = new Joc(t1);
         BuscaminesVista vista  = new MockBuscaminesVista(joc); //Creem un mock de la vista.
-		vista.initVista(); //La vista és genera a partir del mock (no fa res)
-		joc.crearVistaDelJoc(vista); 
-		
+		vista.initVista(); //La vista és genera a partir del mock (no fa res).
+		joc.crearVistaDelJoc(vista);
         joc.clicDret(2, 2); // Posem bandera a la posició 2,2
-        assertTrue(joc.getTauler().getCasella(2, 2).isBandera()); // Comprovem que la bandera s'ha possat
+        assertTrue(joc.isBandera(2,2)); // Comprovem que la bandera s'ha possat
         
-        // Cas on no podrem possar una bandera ja que la casella ja ha estat destapada
-        joc.getTauler().getCasella(0,1).setMina(); // Posem una mina a la casella 0,1
-        joc.getTauler().destapaCasella(1,1); // Destapem la casella 1,1 (per tant tindrá el numero de mines al voltant = 1)
-        assertFalse(joc.getTauler().getCasella(1, 1).isBandera()); // Comprovem que no podem col·locar bandera ja que esta destapada
+        // Cas on fem clic dret sobre una casella que ja té una bandera (es treu la bandera)
+        joc.clicDret(2, 2);
+        assertFalse(joc.isBandera(2,2)); // Comprovem que la bandera s'ha tret.
+        
+        // Cas on no podrem possar una bandera ja que la casella ja ha estat destapada.
+        r = new Random();
+		t1 = new Tauler(r);
+        joc = new Joc(t1);
+        vista  = new MockBuscaminesVista(joc); //Creem un mock de la vista.
+		vista.initVista(); //La vista és genera a partir del mock (no fa res).
+		joc.crearVistaDelJoc(vista);
+		joc.clicEsquerra(2, 2);
+        joc.clicDret(2, 2); // Posem bandera a la posició 2,2 (que esta destapada)
+        assertFalse(joc.isBandera(2,2)); // Comprovem que la bandera s'ha possat
         
     }
 	

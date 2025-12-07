ffc628724b459a04f9c087b389cd096d7f87109a
Ivanguerra13 <167463663+Ivanguerra13@users.noreply.github.com>
2025-12-07 13:29:46 +0100

    Correcció comentaris

--- src/model/MockTauler.java
+++ src/model/MockTauler.java
@@ -1,25 +1,26 @@
-
 package model;
 
 import java.util.Random;
 
 public class MockTauler extends Tauler{
 	
-	// Mock object de la classe Tauler per facilitar els casos de prova que requereixen generació de taulers especifics.
-	// Com la generació de mines és random, necessitem taulers amb les mines posicionades manualment per nosaltres.
+	// Mock object de la classe Tauler per facilitar els casos de prova que requereixen generar taulers específics.
+	// Com que la generació de mines és aleatòria en la classe original, necessitem taulers amb mines
+	// posicionades manualment per poder verificar el funcionament d'altres mètodes.
 	
 	public MockTauler(Random r) {
 		super(r);
 	}
 	
-	
 	@Override
     public void generaMinesRandom(int xPlayer, int yPlayer) {
-        //Fem un override de la funció generaMinesRandom perquè no sigui random i per tant tenir un control d'on es possan i poder realitzar comprovacions a altres mètodes.
-		//Utilitzarem els paràmetres de xPlayer i yPlayer per realitzar els diferents casos (2 casos). Realitzem un tauler predefinit de 30 mines per poder fer la simulació del joc
-		//i comprovar que funciona correctament tot.
+        // Override de la funció generaMinesRandom perquè no sigui aleatòria.
+        // Això ens permet controlar exactament on es col·loquen les mines i així realitzar proves deterministes.
+		// Utilitzem el paràmetre xPlayer per seleccionar diferents configuracions predefinides de mines.
+		// En aquest cas, només fem servir la configuració 1, que genera un tauler fix de 30 mines.
+		
 		switch(xPlayer) {
-		case 1: // Posició del tauler 1 de les mines predefinides per guanyar.
+		case 1: // Posició del tauler 1: mines predefinides per un cas de prova on el jugador pot guanyar.
 			tauler[0][0].setMina();
 			tauler[1][1].setMina();
 			tauler[1][3].setMina();
@@ -50,10 +51,10 @@ public class MockTauler extends Tauler{
 			tauler[12][5].setMina();
 			tauler[12][8].setMina();
 			tauler[12][10].setMina();
-
+			break;
 			
 		default:
+			// Si es passa un valor no previst, no es generen mines (cas segur per defecte).
 		}
 	}
-   
 }

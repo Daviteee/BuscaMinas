fbdbb07590771cb2cbb25651a6dd96bf132be82b
Daviteee <davidmoraleslopez005@gmail.com>
2025-12-08 15:20:45 +0100

    Correcció MockVista

--- src/main/java/es/uab/tqs/buscamines/vista/MockBuscaminesVista.java
+++ src/main/java/es/uab/tqs/buscamines/vista/MockBuscaminesVista.java
@@ -1,21 +1,19 @@
 package es.uab.tqs.buscamines.vista;
 
 import es.uab.tqs.buscamines.controlador.Joc;
+import es.uab.tqs.buscamines.vista.Vista;
 
-public class MockBuscaminesVista extends BuscaminesVista{
+public class MockBuscaminesVista implements Vista{
 
 	//Mock object per a quan realitzem els test de joc no ens mostri la vista, ja que no és necesària.
-	public MockBuscaminesVista(Joc joc) {
-		super(joc);
+	public MockBuscaminesVista() {
 	}
 	
 	//Aquesta funció no fa res ja que no ha d'iniciar cap vista
-	@Override
 	public void initVista() {
 	}
 	
 	//Aquesta funció no fa res perquè només volem que no es mostri la vista quan realitzem els tests de joc
-	@Override
 	public void actualitzar() {		
 	}
 }
